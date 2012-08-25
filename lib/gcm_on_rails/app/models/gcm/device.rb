9fd663f3ba7ff6cce6ff4ecff2ba1be36f7039d0
Joey <joseph.tutela@gmail.com>
2012-08-24 19:35:32 -0700

    rework device to allow multiple devices per user with only one being active

--- lib/gcm_on_rails/app/models/gcm/device.rb
+++ lib/gcm_on_rails/app/models/gcm/device.rb
@@ -1,6 +1,6 @@
 class Gcm::Device < Gcm::Base
   self.table_name = "gcm_devices"
-
+  before_save :only_one_active
   attr_accessible :user_id, :registration_id
   belongs_to :user
   has_many :notifications_devices, class_name: 'Gcm::NotificationsDevice', dependent: :destroy
@@ -8,5 +8,24 @@ class Gcm::Device < Gcm::Base
 
   validates :registration_id, :presence => true
   validates :user, :presence => true
+
+  # Scopes
+  def self.for_user(id)
+    where(user_id: id)
+  end
+
+  def self.not_for(id)
+    where("gcm_devices.id != ?", id)
+  end
+
+  private
+  def only_one_active(device)
+    if device.active
+      Gcm::Device.for_user(device.user_id).not_for(device.id).find_each do |d|
+        d.active = false
+        d.save
+      end
+    end
+  end
 
 end
