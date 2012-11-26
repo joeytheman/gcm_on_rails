3414f746666e29ee04a25167455d90dbf40b8334
joeytheman <joseph.tutela@gmail.com>
2012-11-26 15:26:20 -0800

    add :inverse_of for has_many :notification_devices

--- lib/gcm_on_rails/app/models/gcm/notifications_device.rb
+++ lib/gcm_on_rails/app/models/gcm/notifications_device.rb
@@ -2,7 +2,7 @@ class Gcm::NotificationsDevice < Gcm::Base
   self.table_name = "gcm_notifications_devices"
 
   attr_accessible :notification_id, :device_id
-  belongs_to :notification, :class_name => "Gcm::Notification"
+  belongs_to :notification, :class_name => "Gcm::Notification", :inverse_of => :notifications_devices
   belongs_to :device, :class_name => 'Gcm::Device'
   validates :notification_id, :presence => true
 
